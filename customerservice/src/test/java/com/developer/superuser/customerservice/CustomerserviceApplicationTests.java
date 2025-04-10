dbf88bb46cff1c5a8b6ae4a7ea7431b5eb55aab6
Kuntiarso <gilang.grk17@gmail.com>
2025-04-10 22:30:01 +0700

    feat(customer-service): setup config and dependencies

---
+++ customerservice/src/test/java/com/developer/superuser/customerservice/CustomerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.superuser.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
