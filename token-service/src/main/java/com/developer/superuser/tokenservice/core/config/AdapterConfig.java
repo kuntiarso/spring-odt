c91dc05188ec2a4922348cec6094c55ad9038417
Kuntiarso <gilang.grk17@gmail.com>
2025-08-24 17:11:39 +0700

    feat(token-service): removed unused signature bean

--- token-service/src/main/java/com/developer/superuser/tokenservice/core/config/AdapterConfig.java
+++ token-service/src/main/java/com/developer/superuser/tokenservice/core/config/AdapterConfig.java
@@ -1,10 +1,6 @@
 package com.developer.superuser.tokenservice.core.config;
 
 import com.developer.superuser.shared.project.springodt.sign.Basic;
-import com.developer.superuser.tokenservice.signature.SignatureService;
-import com.developer.superuser.tokenservice.signatureadapter.SignatureEntityMapper;
-import com.developer.superuser.tokenservice.signatureadapter.SignatureRepository;
-import com.developer.superuser.tokenservice.signatureadapter.SignatureServiceAdapter;
 import com.developer.superuser.tokenservice.token.TokenApiService;
 import com.developer.superuser.tokenservice.token.TokenCacheService;
 import com.developer.superuser.tokenservice.tokenadapter.api.SignMapper;
@@ -16,11 +12,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AdapterConfig {
-    @Bean
-    public SignatureService signatureService(SignatureRepository signatureRepository, SignatureEntityMapper signatureEntityMapper) {
-        return new SignatureServiceAdapter(signatureRepository, signatureEntityMapper);
-    }
-
     @Bean
     public TokenApiService tokenApiService(Basic basic, SignMapper signMapper, TokenApi tokenApi) {
         return new TokenApiServiceAdapter(basic, signMapper, tokenApi);
