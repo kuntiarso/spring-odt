f724e41bf3bba33c1023d688619daa50d086c95b
Kuntiarso <gilang.grk17@gmail.com>
2025-09-15 00:51:41 +0700

    feat(db-migration): reset migration scripts

--- db-migration/src/main/resources/db/V1__Create_payment_table.sql
+++ db-migration/src/main/resources/db/V1__Create_payment_table.sql
@@ -1,24 +1,26 @@
 CREATE TABLE IF NOT EXISTS `payment` (
     `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `request_id` VARCHAR(14) NOT NULL,
     `order_id` BIGINT NOT NULL,
     `user_id` BIGINT NOT NULL,
     `type` VARCHAR(20) NOT NULL,
     `gateway` VARCHAR(10) NOT NULL,
-    `amount_value` DECIMAL(19, 2) NOT NULL DEFAULT 0,
-    `amount_currency` VARCHAR(3) NOT NULL DEFAULT 'IDR',
-    `status` VARCHAR(16) NOT NULL DEFAULT 'INITIATED',
+    `amount_value` DECIMAL(19, 2) NOT NULL,
+    `amount_currency` VARCHAR(3) NOT NULL,
+    `status` VARCHAR(16) DEFAULT 'INITIATED',
     `error_code` VARCHAR(10) DEFAULT NULL,
     `error_message` TEXT DEFAULT NULL,
     `paid_at` TIMESTAMP(6) DEFAULT NULL,
-    `created_at` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `created_at` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6),
     `created_by` VARCHAR(100) NOT NULL,
     `updated_at` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `updated_by` VARCHAR(100) NOT NULL,
-    `deleted` BIT(1) NOT NULL DEFAULT 0,
+    `deleted` BOOLEAN DEFAULT FALSE,
     `deleted_at` TIMESTAMP(6) DEFAULT NULL,
     `deleted_by` VARCHAR(100) DEFAULT NULL,
-    `version` BIGINT NOT NULL DEFAULT 0
+    `version` BIGINT DEFAULT 0,
+    CONSTRAINT `idx_payment_req_id` UNIQUE (`request_id`)
 );
 
-CREATE INDEX `idx_ord_id` ON `payment`(`order_id`);
-CREATE INDEX `idx_usr_id` ON `payment`(`user_id`);
+CREATE INDEX `idx_payment_ord_id` ON `payment`(`order_id`);
+CREATE INDEX `idx_payment_usr_id` ON `payment`(`user_id`);
--- db-migration/src/main/resources/db/V2__Create_va_payment_detail_table.sql
+++ db-migration/src/main/resources/db/V2__Create_va_payment_detail_table.sql
@@ -1,32 +1,35 @@
 CREATE TABLE IF NOT EXISTS `va_payment_detail` (
     `payment_id` BIGINT PRIMARY KEY,
+    `request_id` VARCHAR(14) NOT NULL,
     `partner_id` VARCHAR(8) NOT NULL,
     `inquiry_id` VARCHAR(30) DEFAULT NULL,
     `customer_no` VARCHAR(20) NOT NULL,
     `va_no` VARCHAR(20) NOT NULL,
     `va_name` VARCHAR(255) NOT NULL,
-    `billed_amount_value` DECIMAL(19, 2) NOT NULL DEFAULT 0,
-    `billed_amount_currency` VARCHAR(3) NOT NULL DEFAULT 'IDR',
-    `paid_amount_value` DECIMAL(19, 2) NOT NULL DEFAULT 0,
-    `paid_amount_currency` VARCHAR(3) NOT NULL DEFAULT 'IDR',
+    `billed_amount_value` DECIMAL(19, 2) NOT NULL,
+    `billed_amount_currency` VARCHAR(3) NOT NULL,
+    `paid_amount_value` DECIMAL(19, 2) DEFAULT NULL,
+    `paid_amount_currency` VARCHAR(3) DEFAULT NULL,
     `channel` VARCHAR(30) NOT NULL,
     `how_to_pay_page` VARCHAR(255) DEFAULT NULL,
     `how_to_pay_api` VARCHAR(255) DEFAULT NULL,
-    `conf_reusable` BIT(1) NOT NULL DEFAULT 0,
-    `conf_min_amount` DECIMAL(19, 2) NOT NULL DEFAULT 0,
-    `conf_max_amount` DECIMAL(19, 2) NOT NULL DEFAULT 0,
+    `conf_reusable` BOOLEAN DEFAULT FALSE,
+    `conf_min_amount` DECIMAL(19, 2) DEFAULT NULL,
+    `conf_max_amount` DECIMAL(19, 2) DEFAULT NULL,
     `transaction_type` CHAR(1) NOT NULL,
     `expired_at` TIMESTAMP(6) DEFAULT NULL,
-    `status` VARCHAR(16) NOT NULL DEFAULT 'INITIATED',
-    `created_at` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `status` VARCHAR(16) DEFAULT 'INITIATED',
+    `created_at` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP (6),
     `created_by` VARCHAR(100) NOT NULL,
     `updated_at` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `updated_by` VARCHAR(100) NOT NULL,
-    `deleted` BIT(1) NOT NULL DEFAULT 0,
+    `deleted` BOOLEAN DEFAULT FALSE,
     `deleted_at` TIMESTAMP(6) DEFAULT NULL,
     `deleted_by` VARCHAR(100) DEFAULT NULL,
-    `version` BIGINT NOT NULL DEFAULT 0
+    `version` BIGINT DEFAULT 0,
+    CONSTRAINT `idx_va_req_id` UNIQUE (`request_id`),
+    CONSTRAINT `fk_va_payment` FOREIGN KEY (`payment_id`) REFERENCES `payment`(`id`) ON DELETE CASCADE
 );
 
-CREATE INDEX `idx_inq_req_id` ON `va_payment_detail`(`inquiry_id`);
-CREATE INDEX `idx_va_no` ON `va_payment_detail`(`va_no`);
+CREATE INDEX `idx_va_inq_id` ON `va_payment_detail`(`inquiry_id`);
+CREATE INDEX `idx_va_va_no` ON `va_payment_detail`(`va_no`);
